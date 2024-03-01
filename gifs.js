8c3eed722cae3da83a007674f6586636da3bf90e
Rexifer <yadel.adelie@gmail.com>
2024-03-01 13:32:26 -0700

    hack

--- gifs.js
+++ gifs.js
@@ -1,15 +1,13 @@
+let content = document.getElementById('gifs');
 let database;
 let xhr = new XMLHttpRequest();
 xhr.open("GET", "database.json");
 xhr.send();
 xhr.onload = () => {
     database = JSON.parse(xhr.responseText);
-}
-
-let content = document.getElementById('gifs');
-
-for (let gif of database) {
-    let img = document.createElement("img");
-    img.src = gif.url;
-    content.appendChild(img);
+    for (let gif of database) {
+        let img = document.createElement("img");
+        img.src = gif.url;
+        content.appendChild(img);
+    }
 }
