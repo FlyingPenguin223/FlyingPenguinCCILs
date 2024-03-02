d6c8e31e6519fb0a17e7dc0c6a4a0c8ada6a03e6
Rexifer <yadel.adelie@gmail.com>
2024-03-02 09:15:40 -0700

    basically copy the gif db code

--- gifs.js
+++ gifs.js
@@ -1,3 +1,5 @@
+//all of this code blatantly taken from the celeste classic gif database.
+
 let content = document.getElementById('gifs');
 let database;
 let xhr = new XMLHttpRequest();
@@ -5,9 +7,43 @@ xhr.open("GET", "database.json");
 xhr.send();
 xhr.onload = () => {
     database = JSON.parse(xhr.responseText);
-    for (let gif of database) {
-        let img = document.createElement("img");
-        img.src = gif.url;
-        content.appendChild(img);
-    }
 }
+
+function getGifs(searchTags) {
+    return database.filter(gif => {
+        for (let tag of searchTags)
+            if (!gif.tags.includes(tag))
+                return false;
+        return true;
+    });
+}
+
+let textbox = document.getElementById('search');
+let submit = document.getElementById('submit');
+
+textbox.addEventListener("keyup", (e) => {
+    if (e.code === "Enter")
+      submit.click();
+});
+
+submit.addEventListener('click', () => {
+    let searchTags = new Set(textbox.value.split(/\s+/).filter(x => x != ''));
+    if (searchTags.size == 0)
+      return;
+  
+    let content = document.getElementById('gifs');
+    while (content.firstChild) {
+     content.firstChild.remove();
+    }
+  
+    let gifResults = getGifs(searchTags);
+    for (let gif of gifResults) {
+      let img = document.createElement("img");
+      img.src = gif.url;
+      img.title = 'Tags: ' + gif.tags.join(", ");
+      img.width = 256;
+      img.height = 256;
+  
+      content.appendChild(img);
+    }
+});
