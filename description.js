31451b5817a70e26de232b8232f8b156bca0f943
Rexifer <yadel.adelie@gmail.com>
2024-03-13 17:59:18 -0700

    properly handle Response

--- description.js
+++ description.js
@@ -18,7 +18,9 @@ fetch("descriptions/"+filename+".txt").then(Response => {
     if (!Response.ok) {
         words.innerHTML = "There is no description for this gif (or a server error occured).";
     } else {
-        words.innerHTML = Response.text();
+        Response.text().then(text => {
+            words.innerHTML = text;
+        });
     }
 });
 text.appendChild(words);
