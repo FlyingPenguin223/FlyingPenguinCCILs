f701922d9f606d2bf3ab7293dec8f2f4c59e2664
flyingpenguin223 <yadel.adelie@gmail.com>
2025-07-25 08:44:53 -0400

    I should really use a local web server to test

--- gifs.js
+++ gifs.js
@@ -6,44 +6,41 @@ let xhr = new XMLHttpRequest();
 xhr.open("GET", "database.json");
 xhr.send();
 xhr.onload = () => {
-    database = JSON.parse(xhr.responseText);
+	database = JSON.parse(xhr.responseText);
 }
 
 function getGifs(searchTags) {
-    return database.filter(gif => {
-        for (let tag of searchTags)
-			if (tag == "all")
-				return true;
-            if (!gif.tags.includes(tag))
-                return false;
-        return true;
-    });
+	return database.filter(gif => {
+		for (let tag of searchTags) {
+				return gif.tags.includes(tag) || tag == "all"
+		}
+	});
 }
 
 let textbox = document.getElementById('search');
 let submit = document.getElementById('submit');
 
 textbox.addEventListener("keyup", (e) => {
-    if (e.code === "Enter")
-      submit.click();
+	if (e.code === "Enter")
+	  submit.click();
 });
 
 submit.addEventListener('click', () => {
-    let searchTags = new Set(textbox.value.split(/\s+/).filter(x => x != ''));
-    if (searchTags.size == 0)
-      return;
+	let searchTags = new Set(textbox.value.split(/\s+/).filter(x => x != ''));
+	if (searchTags.size == 0)
+	  return;
   
-    let content = document.getElementById('gifs');
-    while (content.firstChild) {
-     content.firstChild.remove();
-    }
+	let content = document.getElementById('gifs');
+	while (content.firstChild) {
+	 content.firstChild.remove();
+	}
   
-    let gifResults = getGifs(searchTags);
-    for (let gif of gifResults) {
-      let img = document.createElement("a");
-      img.href="description.html?lvl="+gif.url
-      img.innerHTML='<img src="gifs/'+gif.url+'" title="Tags: '+gif.tags.join(", ")+'" width=256 height=256>'
+	let gifResults = getGifs(searchTags);
+	for (let gif of gifResults) {
+	  let img = document.createElement("a");
+	  img.href="description.html?lvl="+gif.url
+	  img.innerHTML='<img src="gifs/'+gif.url+'" title="Tags: '+gif.tags.join(", ")+'" width=256 height=256>'
   
-      content.appendChild(img);
-    }
+	  content.appendChild(img);
+	}
 });
